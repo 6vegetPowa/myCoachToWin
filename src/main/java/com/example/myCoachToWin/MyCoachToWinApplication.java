f1247b53af1d9f4a22ed93f3ffc46e0d8815970f
Cyril Millot <cmillot@solocalms.fr>
2023-08-18 19:35:20 +0200

    F

---
+++ src/main/java/com/example/myCoachToWin/MyCoachToWinApplication.java
@@ -0,0 +1,13 @@
+package com.example.myCoachToWin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCoachToWinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCoachToWinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myCoachToWin/MyCoachToWinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myCoachToWin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCoachToWinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
